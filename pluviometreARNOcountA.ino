4881c07f751b1f711e90a24f250e7aaf2fabca2c
Lycee des Andaines <vettierarnaud@gmail.com>
2016-11-12 14:39:27 +0100

    Add files via upload

---
+++ pluviometreARNOcountA.ino
@@ -0,0 +1,36 @@
+int bascules ;
+int compteur ; 
+const int pinD13 = 13;
+int countA ; 
+void setup() {
+ // Initialisation du port série
+  Serial.begin(9600);
+  Serial.println("test pluviometre a bascule");
+  compteur = 0 ;
+  countA=0 ;
+}
+
+void loop() {
+// quand la bscule ne fait rien sa valeur est à 1
+
+    bascules = digitalRead(13);
+   
+  // Serial.print(bascules);
+
+        if (bascules ==0 )
+// si on met bascules=0 on affecte bascules à 0 si l'on veut comparer il faut == alors il ne rentre jamais dans le if il doit affecterla valeur ???
+        {
+          for ( bascules ==0 ; countA<9 ; countA++) 
+              {
+              Serial.print("la boulce for tourne countA=");
+              Serial.println(countA);
+              } 
+    countA=0 ;
+    compteur ++ ;
+     Serial.print("nombre de bascule =");
+     Serial.println(compteur);
+              // fermertue du for
+        }  //fermeture du if
+    
+
+}
---
+++ pluviometreinterruptARNOfonctionne.ino
@@ -0,0 +1,47 @@
+int bascules ;
+int compteur ; 
+const int pinD3 = 3;
+//attazhc interrupt ne fonctionne que sur le pin D2 ou D3
+int  temporisation;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("test pluviometre a bascule");
+  compteur = 0 ;
+  pinMode(pinD3, INPUT_PULLUP); 
+
+ // https://www.arduino.cc/en/Reference/AttachInterrupt
+ //attachInterrupt(digitalPinToInterrupt(numero du pin qui subit l'interrupt) , nom de la fonction d'interruption, que doit regarde l'interruption pour se declancher);
+
+ 
+attachInterrupt(digitalPinToInterrupt(3) , interrupt1, FALLING);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+    bascules = digitalRead(13);
+    // ligne suivante pour verifier que le pluviometre bascule 
+    Serial.print("compteur vaut  ");
+    delay (1000);
+    Serial.println(compteur);
+/* if (bascules ==0  )
+        {
+          Serial.println(compteur);
+
+        }
+*/
+}
+
+void interrupt1 ()
+  {
+        if((millis() - temporisation) > 15 ) 
+        { // debounce of sensor signal / anti-rebond
+          // dans la fonction d'interruption il doit y avoir le moins de commande possible 
+              
+               compteur ++ ;
+               temporisation = millis();
+      }
+        
+  }
+
